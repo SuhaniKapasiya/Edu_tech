25d22b7135a5160d53ed3d86f3fafc92b1beb088
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2024-03-22 13:45:49 +0530

    Subsection and Profile code complete

---
+++ backend/controllers/Profile.js
@@ -0,0 +1,103 @@
+const User = require("../models/User");
+
+exports.updateProfile = async (req, res)=>{
+    try{
+        //get data 
+        const {dateOfBirth="",about="",contactNumber, gender}  = req.body;
+        //get UserId
+        const id = req.user.id;
+        if(!contactNumber || !gender || !id){
+            return res.status(400).json({
+                success:false,
+                message:'All fields are requires',
+            })
+        }
+        //find profile
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+        const profileDetails = await profileId.findById(profileId);
+
+        //update profile 
+        profileDetails.dateOfBirth = dateOfBirth;
+        profileDetails.about = about;
+        profileDetails.gender = gender;
+        profileDetails.contactNumber = contactNumber;
+        await profileDetails.save();
+
+        return res.status(200).json({
+            success:true,
+            message:'Profile Updated Successfully',
+            profileDetails,
+        })
+
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            error:error.message,
+        })
+    }
+}
+
+
+//deleteAccount
+//Explore ->how can we schedule this deletion operation
+exports.deleteAccount =async(req,res)=>{
+    try{
+        //get id 
+        const id = req.user.id;
+        //validation
+        const userDetails = await User.findById(id);
+        if(!userDetails){
+            return res.status(400).json({
+                success:false,
+                message:'User not found',
+            });
+        }
+        //delete profile
+        await Profile.findByIdAndDelete({id:userDetails.additionalDetails});
+        //TODO :HW unenroll user from all enrlled course
+        //delete User
+        await User.findByIdAndUpdate({_id:id});
+       // return response 
+       return res.status(200).json({
+        success:true,
+        message:'User Deleted Successfully'
+       })
+
+
+
+    }catch(error){
+
+        return res.status(500).json({
+            success:false,
+            message:'User cannot be deleted successfully',
+        })
+    }
+
+
+
+
+}
+
+exports.getAllUserDetails = async (req, res) => {
+  try {
+    //get id
+    const id = req.user.id;
+
+    //validation and get user details
+
+    const userDetails = await User.findById(id).populate("additionalDetails").exec();
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "User Data Fetched Successfully",
+      userDetails,
+    });
+  } catch (error) {
+      return res.status(500).json({
+        success:false,
+        message:error.message,
+      })
+  }
+
+};
--- backend/controllers/Subsection.js
+++ backend/controllers/Subsection.js
@@ -1,21 +1,56 @@
 const SubSection =  require("../models/SubSection");
-const Section = require("../model/Section")
+const Section = require("../model/Section");
+const SubSection = require("../models/SubSection");
+const {uploadImageToCloudinary} = require("../utils/imageUploader")
 
 //create SubSection
 
 exports.createSubSection = async (req,res)=>{
     try{
         //fetch data from Req body
+        const {sectionId,title,timeDuration,description} = req.body;
         //extract file/video
+        const video = req.files.videoFile;
         //validation
+        if(!sectionId || !title || !timeDuration || !description  || !video){
+            return res.status(400).json({
+                success:false,
+                message:'All fields are required',
+            })
+        }
         //upload video to cloudinary
+        const uploadDetails = await uploadImageToCloudinary(video,process.env.FOLDER_NAME);
         //create a sub Section
+        const subSectionDetails = await SubSection.create({
+            title:title,
+            timeDuration:timeDuration,
+            description:description,
+            videoUrl:uploadDetails.secure_url,
+        })
         //update section with this sub sectio ObjectId
+        const updatedSection = await Section.findByIdAndUpdate(
+          { _id: sectionId },
+          {
+            $push: {
+              SubSection: subSectionDetails._id,
+            },
+          },
+          { new: true }
+        ).populate('SubSection').exec();
+        console.log("Updated Section:", updatedSection);
+        //HW log updated section here ,after adding populate query
         //return res
-
-
+          return res.status(200).json({
+            success:'Sub Section Created Successfully',
+            updatedSection,
+          })
 
     }catch(error){
-
+       return res.status(500).json({
+         success:false,
+        message:"Internal Server Error",
+        error :error.message,
+       })
     }
 }
+
