fee4849a7170ad48feafa99e0bd077eeeccf125b
shubham jha <116482692+shubhamjha2@users.noreply.github.com>
2024-05-05 15:53:36 +0530

    cource fetching from backend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,101 +1,117 @@
-import React, { useState, useEffect } from "react";
-import { Link, NavLink, matchPath, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
+import { BsChevronDown } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { Link, matchPath, useLocation } from "react-router-dom";
+
 import Logo from "../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../data/Navbar-option";
-import { useSelector } from "react-redux";
-import { BsChevronDown } from "react-icons/bs";
-import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
-import ProfileDropdown from "./Core/auth/ProfileDropdown";
+import { apiConnector } from "../service/apiconnector";
+import { categories } from "../service/apis";
 import { ACCOUNT_TYPE } from "./utils/constants";
+import ProfileDropdown from "../components/Core/auth/ProfileDropdown";
 
-const subLinks = [
-  {
-    title: "python",
-    link: "/catalog/python",
-  },
-  {
-    title: "web dev",
-    link: "/catalog/web-dev",
-  },
-];
 function Navbar() {
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
   const location = useLocation();
+
+  const [subLinks, setSubLinks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchSublinks = async () => {
+    try {
+      const result = await apiConnector("GET", categories.CATEGORIES_API);
+      // console.log("Printing Sublinks result:", result);
+      setSubLinks(result.data.data);
+      // console.log(result.data.data);
+    } catch (error) {
+      console.log("Could not fetch the category list");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSublinks();
+  }, []);
+
+  console.log("sub links", subLinks);
+
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
   };
 
-  const { token } = useSelector((state) => state.auth);
-  const { user } = useSelector((state) => state.profile);
-  const { totalItems } = useSelector((state) => state.cart);
-  // const [subLinks, setsubLinks] = useState([]);
-  // useEffect(() => {
-  //   ;(async () => {
-  //     setLoading(true);
-  //     try {
-  //       const res = await apiConnector("GET", categories.CATEGORIES_API);
-  //       setSubLinks(res.data.data);
-  //     } catch (error) {
-  //       console.log("Could not fetch Categories.", error);
-  //     }
-  //     setLoading(false);
-  //   })();
-  // }, []);
   return (
-    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-500">
+    <div
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+        location.pathname !== "/" ? "bg-richblack-800" : ""
+      } transition-all duration-200`}
+    >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
-        {/* logo */}
+        {/* Logo */}
         <Link to="/">
-          <img src={Logo} alt="ourlogo" width={160} height={32} />
+          <img src={Logo} alt="Logo" width={160} height={32} loading="lazy" />
         </Link>
-        {/* navliks  home about us*/}
+        {/* Navigation links */}
         <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
-            {NavbarLinks.map((item, index) => (
+            {NavbarLinks.map((link, index) => (
               <li key={index}>
-                {item.title === "Catalog" ? (
-                  <div
-                    className={`group relative flex cursor-pointer items-center gap-1 ${
-                      matchRoute("/catalog/:catalogName")
-                        ? "text-yellow-25"
-                        : "text-richblack-25"
-                    }`}
-                  >
-                    <p>{item.title}</p>
-                    <BsChevronDown />
-                    <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
-                      <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
-                      {subLinks.length ? (
-                        <>
-                          {subLinks.map((subLink, i) => (
-                            <Link
-                              to={`${subLink.link}`}
-                              className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
-                              key={i}
-                            >
-                              <p>{subLink.title}</p>
-                            </Link>
-                          ))}
-                        </>
-                      ) : (
-                        <p className="text-center">No Courses Found</p>
-                      )}
+                {link.title === "Catalog" ? (
+                  <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks && subLinks.length ? (
+                          <>
+                            {subLinks.map((subLink, index) => (
+                              <Link
+                                to={`catalog/${subLink.name
+                                  .split(" ")
+                                  .join("-")
+                                  .toLowerCase()}`}
+                                className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                key={index}
+                              >
+                                <p>{subLink.name}</p>
+                              </Link>
+                            ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
                     </div>
-                  </div>
+                  </>
                 ) : (
-                  <NavLink
-                    className={({ isActive }) =>
-                      `${isActive ? "text-yellow-25" : "text-richblack-25"}`
-                    }
-                    to={item.path}
-                  >
-                    {item.title}
-                  </NavLink>
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
                 )}
               </li>
             ))}
           </ul>
         </nav>
-
+        {/* Login / Signup / Dashboard */}
         <div className="hidden items-center gap-x-4 md:flex">
           {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
             <Link to="/dashboard/cart" className="relative">
